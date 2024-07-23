71ad7978ae77793a88d436dca0cc27660c3e17ca
Creator X <godofcreation980316@gmail.com>
2024-07-23 16:14:44 +0800

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,18 @@
+#test2
+print ('  ')
+the_answer_you_want = input('Current temperature in degrees Celsius?  :  ')
+print ('  ')
+the_answer_you_want = int(the_answer_you_want)
+Fahrenheit = the_answer_you_want*1.8+32
+print ('Current temperature (Fahrenheit) is : ', Fahrenheit)
+
+
+
+
+
+
+
+
+
+
+
